b47a9477aa1bba3fd8c2fb1ba900fc48aba51cea
CS70 Rabbit <mshen@cs.hmc.edu>
2018-03-14 00:29:46 -0700

    created doPost function in RegisterServlet, added username requirements

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -17,4 +17,21 @@ public class RegisterServlet extends HttpServlet {
 
    request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
  }
+
+ @Override
+ public void doPost(HttpServletRequest request, HttpServletResponse response)
+     throws IOException, ServletException {
+
+   String username = request.getParameter("username");
+   String password = request.getParameter("password");
+
+   if (!username.matches("[\\w*\\s*]*")) {
+     request.setAttribute("error", "Please enter only letters, numbers, and spaces.");
+     request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+     return;
+   }
+
+   response.getWriter().println("<p>Username: " + username + "</p>");
+   response.getWriter().println("<p>Password: " + password + "</p>");
+ }
 }
