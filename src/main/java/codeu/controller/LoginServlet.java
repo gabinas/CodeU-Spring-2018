5c5d916628ce6656b6e476ede6c1b7d3d113da2d
Gabriela Lugo <gabrielalugobaez@hotmail.com>
2018-03-14 23:50:44 -0700

    Update LoginServletTest

--- src/main/java/codeu/controller/LoginServlet.java
+++ src/main/java/codeu/controller/LoginServlet.java
@@ -71,12 +71,12 @@ public class LoginServlet extends HttpServlet {
 
     if (userStore.isUserRegistered(username)) {
       User user = userStore.getUser(username);
-  	  if(password.equals(user.getPassword())){
-  		  request.getSession().setAttribute("user",username);
-  		  response.sendRedirect("/conversations");
-  	  }else{
-  		  request.setAttribute("error","Invalid password.");
-  		  request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request,response);
+      if(password.equals(user.getPassword())){
+        request.getSession().setAttribute("user",username);
+        response.sendRedirect("/conversations");
+      }else{
+        request.setAttribute("error","Invalid password.");
+        request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request,response);
   	  }
     }else{
   		request.setAttribute("error", "That username was not found.");
