6bbc638fe2c340dfb79e713de45b33ad3209327e
K Gabriela Lugo <gabrielalugobaez@hotmail.com>
2018-03-15 15:40:51 -0700

    Fixed style

--- src/main/java/codeu/controller/LoginServlet.java
+++ src/main/java/codeu/controller/LoginServlet.java
@@ -71,14 +71,14 @@ public class LoginServlet extends HttpServlet {
 
     if (userStore.isUserRegistered(username)) {
       User user = userStore.getUser(username);
-      if(password.equals(user.getPassword())){
+      if (password.equals(user.getPassword())) {
         request.getSession().setAttribute("user",username);
         response.sendRedirect("/conversations");
-      }else{
+      } else {
         request.setAttribute("error","Invalid password.");
         request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request,response);
       }
-    }else{
+    } else {
       request.setAttribute("error", "That username was not found.");
       request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request,response);
     }
