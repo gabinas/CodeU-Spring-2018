225cc0b5a54ac3df2c8fd5aa79249dd9cd348d0e
Martin Landin <martinlandin@Martins-MacBook-Air.local>
2018-04-02 11:28:15 -0700

    Added servlet class for profile pages

---
+++ src/main/java/codeu/controller/ProfilePageServlet.java
@@ -0,0 +1,50 @@
+package codeu.controller;
+
+import codeu.model.store.basic.UserStore;
+import codeu.model.store.basic.MessageStore;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+*Servlet class responsible for User profile pages.
+*/
+
+public class ProfilePagesServlet extends HttpServlet {
+
+  // Store class that gives access to Users
+  private UserStore userStore;
+
+  // Store class that gives access to Messages.
+  private MessageStore messageStore;
+
+/**
+  * This function fires when a user navigates to the profile page. It grabs the user name
+  * from the URL, finds the corresponding User, and fetches the user info.
+  * It then forwards to profile.jsp for rendering
+  */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+
+      String requestUrl = request.getRequestURI();
+      String userName = requestUrl.substring("/profile/".length());
+
+      User user = userStore.getUser(username);
+
+      if ( user == null) {
+        // couldn't find user, redirect to home page
+        response.sendRedirect("/index");
+        return;
+      }
+
+      request.getRequestDispatcher("/WEB-INF/view/profile.jsp").forward(request, response);
+
+
+
+    }
+}
